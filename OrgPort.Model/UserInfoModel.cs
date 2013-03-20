1bf9b58ee997bbca96fe79be9df2c275d27aee46
igorezigorez <igorezigorez@gmail.com>
2013-03-21 00:54:12 +0300

    data access begin

---
+++ OrgPort.Model/UserInfoModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrgPort.Model
+{
+    public class UserInformationModel
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string PhotoLink { get; set; }
+        public string AvatarLink { get; set; }
+        public int Rating { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public IEnumerable<UserRole> Roles { get; set; }
+        public IEnumerable<UserInformationModel> RelatedUsers { get; set; }
+    }
+
+    public enum UserRole
+    {
+        Admin,
+        Teacher,
+        Pupil,
+        Parent,
+        User
+    }
+}
